1fe806094f9b3abde3932b59ba1c0265b3a23d25
lndsilva <lndsilva@hotmail.com>
2024-06-02 16:51:41 -0300

    criando modal parte 2

--- src/screens/AddTask.jsx
+++ src/screens/AddTask.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Modal, View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text } from "react-native";
+
+import commonStyles from "../commonStyles";
 
 export default class AddTask extends Component {
     render() {
@@ -10,13 +12,14 @@ export default class AddTask extends Component {
                 onRequestClose={this.onCancel}
                 animationType="slide"
             >
-                <TouchableWithoutFeedback
-                    onPress={this.props.onCancel}
-                >
-                    <View style={styles.background}>
-
-                    </View>
-
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova tarefa</Text>
+                </View>
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
                 </TouchableWithoutFeedback>
             </Modal>
 
@@ -29,6 +32,18 @@ const styles = StyleSheet.create(
         background: {
             flex: 1,
             backgroundColor: 'rgba(0,0,0,0.7)'
+        },
+        container: {
+            flex: 1,
+            backgroundColor: '#FFF',
+        },
+        header: {
+            fontFamily: commonStyles.fontFamily,
+            backgroundColor: commonStyles.colors.today,
+            color: commonStyles.colors.secundary,
+            textAlign: 'center',
+            padding: 15,
+            fontSize: 18,
         }
     }
 )
